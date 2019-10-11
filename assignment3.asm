35df130f1e26da80d9befa490851324281c008e5
Jacob Moulton <Jacob.Moulton@gmail.com>
2019-10-10 21:34:58 -0600

    press and understand

--- assignment3.asm
+++ assignment3.asm
@@ -14,8 +14,8 @@ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
     
     org 0x00	;declare program placement
 
-X equ H'0020'	    ; declare variable X at location H'0020'
-Y equ H'0021'	    ; declare variable Y at location H'0021'
+wait_input equ H'0020'	    ; declare variable X at location H'0020'
+wait_helper equ H'0021'	    ; declare variable Y at location H'0021'
     
 main:
     
@@ -24,32 +24,43 @@ main:
     
 delay:
     nop
-loop1:
-    movlw d'210' ; set X to 194
-    movwf X
 loop2:
     movlw d'255' ; set Y to 255
-    movwf Y
+    movwf wait_helper
 wait:
     nop ;wait a while
     nop
     nop
     nop
     nop
-    decfsz Y, F; if Y is not yet 0, decrease it and wait again.
+    decfsz wait_helper, F; if Y is not yet 0, decrease it and wait again.
     goto wait
-    decfsz X, F; if Y is zero, check to see if X is 0. If it is not, go back to reset Y and wait some more. 
+    decfsz wait_input, F; if Y is zero, check to see if X is 0. If it is not, go back to reset Y and wait some more. 
     goto loop2
     return ; finish the delay
     
 loop:
-    bsf PORTB, 0 ; turn on blue light
-    bcf PORTC, 0 ; turn off red light
-    call delay ; go wait
     bcf PORTB, 0 ; turn off blue light
-    bsf PORTC, 0 ; turn on red light
+    BTFSS PORTC, 0 ; start to look for hold if set, otherwise, rest
+    goto loop
+    
+    movlw d'100' ; set X to 194
+    movwf wait_input
+    call delay
+    
+    BTFSS PORTC, 0 ; if still held, light the led, otherwise, goto loop
+    goto loop
+    
+    
+    bsf PORTB, 0 ; turn on blue light
+    
+    movlw d'250' ; set X to 194
+    movwf wait_input
     call delay ; go wait
+        
     goto loop ; do it again
+    
+    
     return ; should never hit
     
 init:
@@ -60,7 +71,7 @@ init:
     bsf STATUS, RP0
     bcf STATUS, RP1
     bcf TRISB, 0
-    bcf TRISC, 0
+    bsf TRISC, 0
     bcf STATUS, RP0
     bcf STATUS, RP1
     return
