753147805264d0de30111cf398893000e609bded
Jacob Moulton <Jacob.Moulton@gmail.com>
2019-11-10 20:39:39 -0700

    add labels and call appropriately

--- ATD.asm
+++ ATD.asm
@@ -10,38 +10,45 @@ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
     
 org 0x00	;declare program placement
     
-    
-    
-;===== Configure Micro controller ======
-bsf STATUS, RPO ;select bank 3
-bsf STATUS, RP1
 
-CLRF ANSEL	 ; set all to digital
-clrf ANSELH	 ; set all to digital
-bsf ANSELH, 7    ; set ANS 8 to analog
-
-bcf STATUS, RP1; select bank 1
-
-clrf TRISB
-bsf TRISB, 2 ; set RB2 as the only input
-clrf TRISD ; set all of port D as output
-clrf ADCON1; set left aligned, and vss and vdd for reference voltages
-
-bcf STATUS, RPO ; select bank 0
-
-clrf ADCON0
-movlw b'00100011'
-movwf ADCON0    ;set clock source,channel (ANS8), GO!, enable for  ATD
-    
-    
-
-    
     
 main:
+    call init
+    goto set_lights
+    
+
+set_lights:
+    bsf ADCON0, GO
     BTFSC ADCON0, GO
     goto $-1 ; Wait till GO is clear.
-    
+
     movlw ADRESH
     movwf PORTD
-    bsf ADCON0, GO
-    goto main
+    goto set_lights
+    
+    end ;should never hit
+    
+    
+init:
+    bsf STATUS, RP0
+    bsf STATUS, RP1
+
+    CLRF ANSEL	 ; set all to digital
+    clrf ANSELH	 ; set all to digital
+    bsf ANSELH, 7    ; set ANS 8 to analog
+
+    bcf STATUS, RP1; select bank 1
+
+    clrf TRISB
+    bsf TRISB, 2 ; set RB2 as the only input
+    clrf TRISD ; set all of port D as output
+    clrf ADCON1; set left aligned, and vss and vdd for reference voltages
+
+    bcf STATUS, RPO ; select bank 0
+
+    clrf ADCON0
+    movlw b'00100001'
+    movwf ADCON0    ;set clock source,channel (ANS8), enable for  ATD
+    return
+    
+    
